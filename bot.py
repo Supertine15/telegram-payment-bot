9ead3cbda847ac65d3b05ff146c335439f4a0872
Supertine15 <shadhonaroy365@gmail.com>
2025-03-09 11:50:20 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+import os
 
-TOKEN = "7816572849:AAGq-ZO4r5-4_9mimdkNK7_XEvN1Txm0Gkw"
+# Get bot token from environment variable
+TOKEN = os.getenv("TOKEN")
 
 def start(update: Update, context: CallbackContext) -> None:
-    keyboard = [
-        [InlineKeyboardButton("Send Money", callback_data="send_money")],
-    ]
+    keyboard = [[InlineKeyboardButton("Send Money", callback_data="send_money")]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text("Welcome to Auto Payment Bot!", reply_markup=reply_markup)
 
@@ -23,7 +23,7 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CallbackQueryHandler(button_click))
+    dp.add_handler(CallbackQueryHandler(button_click))  # ✅ CallbackQueryHandler যুক্ত করা হয়েছে
 
     updater.start_polling()
     updater.idle()
